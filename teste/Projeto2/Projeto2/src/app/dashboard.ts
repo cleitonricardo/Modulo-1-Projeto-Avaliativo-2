7dd79f6e8ddc0ed4a81e18ab30d2887fca9a6138
Cleiton Ricardo Spagnol <cleitonricardo12@gmail.com>
2022-05-23 21:13:11 -0300

    inicio de tudo

---
+++ teste/Projeto2/Projeto2/src/app/dashboard.ts
@@ -0,0 +1,15 @@
+export class Dashboard {
+id:string;
+apelido:string;
+local:string;
+marca:string;
+modelo:string;
+
+constructor (id:string, apelido:string,local:string,marca:string,modelo:string){
+    this.id=id;
+    this.apelido=apelido;
+    this.local=local;
+    this.marca=marca;
+    this.modelo=modelo;
+}
+}
---
+++ teste/Projeto2/Projeto2/src/app/unidades/unidades.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'esp-unidades',
+  templateUrl: './unidades.component.html',
+  styleUrls: ['./unidades.component.scss'],
+  
+})
+export class UnidadesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
