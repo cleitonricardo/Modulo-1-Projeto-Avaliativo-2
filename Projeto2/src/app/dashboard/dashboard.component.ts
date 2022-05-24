3c72a6b1972840a8c892b2beb76308baa00db1c7
Cleiton Ricardo Spagnol <101300606+cleitonricardo@users.noreply.github.com>
2022-05-24 09:19:50 -0300

    Alterado a pasta raiz

---
+++ Projeto2/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Dashboard } from './dashboard';
+
+
+
+
+@Component({
+  selector: 'esp-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+
+export class DashboardComponent implements OnInit {
+ 
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+
+    }
+}
+
