3c64164f632329f57febc4eb67a405972ca259c3
Cleiton Ricardo Spagnol <cleitonricardo12@gmail.com>
2022-05-29 16:17:26 -0300

    Realizado modificações para salvar as Gerações

--- src/app/consumo/consumo.component.ts
+++ src/app/consumo/consumo.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,26 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './consumo.component.html',
   styleUrls: ['./consumo.component.scss']
 })
-export class ConsumoComponent implements OnInit {
 
-  constructor() { }
+
+export class ConsumoComponent implements OnInit {
+  geracao:any;
+  private apiUrl ="http://localhost:3000/geracoes";
+  constructor( private http:HttpClient) {
+    this.geracao={
+      apelido:'',
+      data:'',
+      totalGerado:'',}
+      
+   }
 
   ngOnInit(): void {
+    this.geracao=[""]
   }
+
+  Salva(){
+    return this.http.post(this.apiUrl, this.geracao).subscribe((result)=>{
+      alert("Geração salva com sucesso")
+  })}
 
 }
